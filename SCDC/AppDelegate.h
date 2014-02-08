f7eb8ccbc55333e0fe6882b838b66a6547fb3619
Distraught63 <distraught63@gmail.com>
2014-02-08 15:29:59 -0500

    Initial Commit

---
+++ SCDC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SCDC
+//
+//  Created by Leen  on 2/8/14.
+//  Copyright (c) 2014 GAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SCDC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SCDC
+//
+//  Created by Leen  on 2/8/14.
+//  Copyright (c) 2014 GAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
