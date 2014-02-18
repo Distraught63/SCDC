db7fb9b99e3e9a6d6bfb029347e1a48a8ec4cf85
Leen Khalid <distraught63@gmail.com>
2014-02-17 19:04:37 -0500

    Added PassCode View

---
+++ SCDC/PassCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  PassCodeViewController.h
+//  SCDC
+//
+//  Created by Leen  on 2/17/14.
+//  Copyright (c) 2014 GAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PassCodeViewController : UIViewController
+
+@end
---
+++ SCDC/PassCodeViewController.m
@@ -0,0 +1,38 @@
+//
+//  PassCodeViewController.m
+//  SCDC
+//
+//  Created by Leen  on 2/17/14.
+//  Copyright (c) 2014 GAI. All rights reserved.
+//
+
+#import "PassCodeViewController.h"
+
+@interface PassCodeViewController ()
+
+@end
+
+@implementation PassCodeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
