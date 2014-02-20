0d14997b6a3df79a12be7783b7b1f3fb63122a97
Leen Khalid <distraught63@gmail.com>
2014-02-19 22:12:27 -0500

    Working on getting the buttons to work.

---
+++ SCDC/MainMenuViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainMenuViewController.h
+//  SCDC
+//
+//  Created by Leen  on 2/17/14.
+//  Copyright (c) 2014 GAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+- (IBAction)unwindMainMenu:(UIStoryboardSegue *)segue;
+
+@end
---
+++ SCDC/MainMenuViewController.m
@@ -0,0 +1,45 @@
+//
+//  MainMenuViewController.m
+//  SCDC
+//
+//  Created by Leen  on 2/17/14.
+//  Copyright (c) 2014 GAI. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+
+- (IBAction) unwindMainMenu:(UIStoryboardSegue *)segue
+{
+    
+}
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
