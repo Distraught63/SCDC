fdf492673685d38b827a2ee705f7b40ba5c46f53
Veena Dali <VeenaDali@gmail.com>
2014-04-19 18:28:48 -0400

    iPad storyboard included

---
+++ SCDC/MainMenuiPadViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainMenuiPadViewController.h
+//  SCDC
+//
+//  Created by Veena Dali on 4/19/14.
+//  Copyright (c) 2014 GAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuiPadViewController : UITableViewController
+
+- (IBAction)unwindMainMenuiPad:(UIStoryboardSegue *)segue;
+
+@end
+
---
+++ SCDC/MainMenuiPadViewController.m
@@ -0,0 +1,54 @@
+//
+//  MainMenuiPadViewController.m
+//  SCDC
+//
+//  Created by Veena Dali on 4/19/14.
+//  Copyright (c) 2014 GAI. All rights reserved.
+//
+
+#import "MainMenuiPadViewController.h"
+
+@interface MainMenuiPadViewController ()
+
+@end
+
+@implementation MainMenuiPadViewController
+- (IBAction) unwindMainMenuiPad:(UIStoryboardSegue *)segue
+{
+    
+}
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIColor *background = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"BusImage.png"]];
+    self.view.backgroundColor = background;
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(IBAction) openWebsite:(id)sender
+{
+    NSURL *url = [NSURL URLWithString:@"http://www.scdcorp.org/"];
+    [[UIApplication sharedApplication] openURL:url];
+}
+
+
+
+@end
